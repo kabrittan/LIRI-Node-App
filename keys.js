12ef39d5508497af2d9690045d7d763d9d83ca91
Kyle Brittan <kabrittan@gmail.com>
2018-04-02 17:47:13 -0700

    Initial Commit

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log("this is loaded");
+
+exports.twitter = {
+  consumer_key: process.env.qYMOFSGf99aW2hvLYNQMxiKQy,
+  consumer_secret: process.env.Aiie7S91u2NJSSrFM5v00WfYk47HMnrOcnfbDsyem5cwVrMjuL,
+  access_token_key: process.env.980590160818487296-1sIHRh9PPMp4OdVaRqN0r79rgIKnqVy,
+  access_token_secret: process.env.920iE1ASwzbRdUTLD0YSZO1trs7qRDbxCo6YBBsuadvD2
+};
+
+exports.spotify = {
+  id: process.env.19e77e52f1cf4ac3aaf0696aba38ca59,
+  secret: process.env.d7a2d854321c4f599b70fe6ac538886d
+};
