1bde0b612bfcace835867706a376877b90c0d70d
Kyle Brittan <kabrittan@gmail.com>
2018-04-06 15:08:18 -0700

    Initial LIRI Code

--- keys.js
+++ keys.js
@@ -1,13 +1,13 @@
 console.log("this is loaded");
 
 exports.twitter = {
-  consumer_key: process.env.qYMOFSGf99aW2hvLYNQMxiKQy,
-  consumer_secret: process.env.Aiie7S91u2NJSSrFM5v00WfYk47HMnrOcnfbDsyem5cwVrMjuL,
-  access_token_key: process.env.980590160818487296-1sIHRh9PPMp4OdVaRqN0r79rgIKnqVy,
-  access_token_secret: process.env.920iE1ASwzbRdUTLD0YSZO1trs7qRDbxCo6YBBsuadvD2
+  consumer_key: "qYMOFSGf99aW2hvLYNQMxiKQy",
+  consumer_secret: "Aiie7S91u2NJSSrFM5v00WfYk47HMnrOcnfbDsyem5cwVrMjuL",
+  access_token_key: "980590160818487296-1sIHRh9PPMp4OdVaRqN0r79rgIKnqVy",
+  access_token_secret: "920iE1ASwzbRdUTLD0YSZO1trs7qRDbxCo6YBBsuadvD2"
 };
 
 exports.spotify = {
-  id: process.env.19e77e52f1cf4ac3aaf0696aba38ca59,
-  secret: process.env.d7a2d854321c4f599b70fe6ac538886d
+  id: "19e77e52f1cf4ac3aaf0696aba38ca59",
+  secret: "d7a2d854321c4f599b70fe6ac538886d"
 };
--- liri.js
+++ liri.js
@@ -1,6 +1,20 @@
-require("dotenv").config();
-require("./keys.js");
-
-
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+const env = require("dotenv").config();
+const keys = require('./keys.js');
+const inquire = require('inquirer');
+const moment = require('moment')
+const Twitter = require('twitter');
+const Spotify = require('node-spotify-api');
+const request = require("request");
+const fs = require('fs');
+
+var command = process.argv[2];
+var spotifyID = "";
+var spotifySecret = "";
+var omdbKey = "trilogy";
+
+
+var song;
+var movie;
+var command;
