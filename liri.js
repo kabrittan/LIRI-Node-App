12912d013fd163d75e9700217c06e566c257bd17
Kyle Brittan <kabrittan@gmail.com>
2018-04-08 15:39:38 -0700

    Do What it Says

--- liri.js
+++ liri.js
@@ -1,38 +1,36 @@
-var env = require("dotenv").config();
+require("dotenv").config();
 var keys = require("./keys.js");
 var inquirer = require("inquirer");
 var request = require("request");
 var fs = require("fs");
 var Spotify = require("node-spotify-api");
 var Twitter = require("twitter");
-var moment = require("moment");
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
-var omdbKey = "trilogy";
 
+var myLiri = "";
+var liriRespond = "";
 var song;
 var movie;
 var command;
 
-function postTweet() {
+if (process.argv[2] === "do-what-it-says") {
+    console.log(process.argv[2]);
+    fs.readFile("./random.txt", "utf8", function (err, data){
+        if (err) throw err;
+        var array = data.split(",");
 
-    var tweetem = { screen_name: "coderkyle64" };
-    client.get("statuses/user_timeline", tweetem, function(error, tweets, response) {
-         if (!error) {
-            for (let i = 1; i < 20; i++) {
-                console.log("Tweet # " + i + "\t" + tweets[i].text + "\n Time: \t" + tweets[i].created_at + "\n\n"); 
-
-                fs.appendFile("log.txt",  moment() + "\n\n\t\tTweet# :" + i + "'" + tweets[i].text + "   "  , function(err) {
-                    if (err) {
-                        return console.log(err)
-                    }
-                    console.log("Text file was updated!");
-                });
+        myLiri = array[0];
+        liriRespond = array[1];
+        console.log(myLiri);
+        console.log(liriRespond);
+        fs.appendFile("./log.txt", process.argv[2] + " led to: ", "utf8", function(err, data) {
+            if (err) {
+                console.log(err);
             }
-        } else { 
-             console.log(error);
-        }
+        });
+        liriAction(myLiri, liriRespond);
     });
+} else {
+    myLiri = process.argv[2];
 }
-
-postTweet();
