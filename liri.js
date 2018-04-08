48b735731ae5e8d20c4f4d0bedfbfc846a753465
Kyle Brittan <kabrittan@gmail.com>
2018-04-08 16:13:30 -0700

    Add Spotify

--- liri.js
+++ liri.js
@@ -33,4 +33,53 @@ if (process.argv[2] === "do-what-it-says") {
     });
 } else {
     myLiri = process.argv[2];
+    liriRespond = process.argv.slice(3).join(" ");
+    liriAction(myLiri, liriRespond);
+}
+
+function liriAction(myLiri, liriRespond) {
+    fs.appendFile("./log.txt", myLiri + "," + liriRespond + "\n", "utf8", function(err, data) {
+        if (err) {
+            console.log(err);
+        }
+    });
+
+    if (myLiri === "spotify-this-song") {
+        if (liriRespond === "") {
+            liriRespond = "women";
+        }
+        spotify.search({ type: "track", query: liriRespond, limit: 1 }, function (err, data) {
+            if (err) {
+                return console.log("Try something else!" + err);
+            } else {
+                concole.log("Artist: " + JSON.stringify(data.tracks.items[0].artists[0].name));
+                concole.log("Song: " + JSON.stringify(data.tracks.items[0].name));
+                concole.log("URL: " + JSON.stringify(data.tracks.items[0].preview_url));
+                concole.log("Album: " + JSON.stringify(data.tracks.items[0].album.name));
+                return;
+            }
+        });
+    }
+
+    else if (myLiri === "my-tweets") {
+        var myTweets = { screen_name: "coderkyle64" };
+        client.get("statuses/user_timeline", myTweets, function (error, tweets, response) {
+            if (!error) {
+                for (i = 0; i < tweets.length; i++) {
+                    console.log("Tweet #" = (i + 1) + ":" + tweets[i].text);
+                }
+            }
+        });
+    }
+    else if (myLiri === "movie-this") {
+        if (liriRespond === "") {
+            liriRespond = "Grown Ups";
+        }
+        var queryUrl = "http://www.omdbapi.com/?t=" + liriRespond + "&y=&plot=short&apikey=trilogy";
+        request(queryUrl, function(error, response, body) {
+            if (!error && response.statusCode === 200){
+                movie +JSON.parse(body);
+            }
+        })
+    }
 }
