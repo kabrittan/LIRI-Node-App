a2e2f94b5baf0a034002561c129eda2a4eb725cb
Kyle Brittan <kabrittan@gmail.com>
2018-04-08 17:39:15 -0700

    Fixed OMDB

--- liri.js
+++ liri.js
@@ -1,3 +1,4 @@
+//Set up required packages and variables
 require("dotenv").config();
 var keys = require("./keys.js");
 var inquirer = require("inquirer");
@@ -7,13 +8,14 @@ var Spotify = require("node-spotify-api");
 var Twitter = require("twitter");
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
-
+//Special variables
 var myLiri = "";
 var liriRespond = "";
 var song;
 var movie;
 var command;
-
+ 
+//"do-what-it-says"
 if (process.argv[2] === "do-what-it-says") {
     console.log(process.argv[2]);
     fs.readFile("./random.txt", "utf8", function (err, data){
@@ -24,7 +26,8 @@ if (process.argv[2] === "do-what-it-says") {
         liriRespond = array[1];
         console.log(myLiri);
         console.log(liriRespond);
-        fs.appendFile("./log.txt", process.argv[2] + " led to: ", "utf8", function(err, data) {
+        //Writing to the log.txt file
+        fs.appendFile("./log.txt", process.argv[2] + ": ", "utf8", function(err, data) {
             if (err) {
                 console.log(err);
             }
@@ -36,7 +39,7 @@ if (process.argv[2] === "do-what-it-says") {
     liriRespond = process.argv.slice(3).join(" ");
     liriAction(myLiri, liriRespond);
 }
-
+//Meat and potatoes of this app
 function liriAction(myLiri, liriRespond) {
     fs.appendFile("./log.txt", myLiri + "," + liriRespond + "\n", "utf8", function(err, data) {
         if (err) {
@@ -50,12 +53,12 @@ function liriAction(myLiri, liriRespond) {
         }
         spotify.search({ type: "track", query: liriRespond, limit: 1 }, function (err, data) {
             if (err) {
-                return console.log("Try something else!" + err);
+                return console.log("Try something else, because " + err);
             } else {
-                concole.log("Artist: " + JSON.stringify(data.tracks.items[0].artists[0].name));
-                concole.log("Song: " + JSON.stringify(data.tracks.items[0].name));
-                concole.log("URL: " + JSON.stringify(data.tracks.items[0].preview_url));
-                concole.log("Album: " + JSON.stringify(data.tracks.items[0].album.name));
+                console.log("Artist(s): " + JSON.stringify(data.tracks.items[0].artists[0].name));
+                console.log("Song: " + JSON.stringify(data.tracks.items[0].name));
+                console.log("Preview URL: " + JSON.stringify(data.tracks.items[0].preview_url));
+                console.log("Album: " + JSON.stringify(data.tracks.items[0].album.name));
                 return;
             }
         });
@@ -66,28 +69,24 @@ function liriAction(myLiri, liriRespond) {
         client.get("statuses/user_timeline", myTweets, function (error, tweets, response) {
             if (!error) {
                 for (i = 0; i < tweets.length; i++) {
-                    console.log("Tweet #" = (i + 1) + ":" + tweets[i].text);
+                    console.log("\n\tTweet # " + (i + 1) + ": " + tweets[i].text);
                 }
             }
         });
     }
     else if (myLiri === "movie-this") {
         if (liriRespond === "") {
-            liriRespond = "Grown Ups";
+            liriRespond = "Mr Nobody";
         }
         var queryUrl = "http://www.omdbapi.com/?t=" + liriRespond + "&y=&plot=short&apikey=trilogy";
         request(queryUrl, function(error, response, body) {
             if (!error && response.statusCode === 200){
-                movie + JSON.parse(body);
-                console.log(movie.Title);
+                movie = JSON.parse(body);
+                console.log("Title: " + movie.Title);
                 console.log("Year: " + movie.Year);
-                if (movie.Ratings[0] !== undefined) {
-                    console.log("IMDB Rating: " + movie.Ratings[0].Value);
-                }
-                if (movie.Ratings[1] !== undefined) {
-                    concole.log("No rating provided.");
-                }
-                console.log("Location: " + movie.Country);
+                console.log("IMDB Rating: " + movie.Ratings[0].Value);
+                console.log("Rotten Tomatoes Rating: " + movie.Ratings[1].Value);
+                console.log("Country: " + movie.Country);
                 console.log("Language: " + movie.Language);
                 console.log("Plot: " + movie.Plot);
                 console.log("Actors: " + movie.Actors);
@@ -96,11 +95,11 @@ function liriAction(myLiri, liriRespond) {
     } else {
         if (myLiri !== "do-what-it-says" || "spotify-this-song" || "my-tweets" || "movie-this") {
         console.log("The only valid commands for this application are as follows:");
-        console.log("do-what-it-says" + " to add any command to the random text file.");
-        console.log("spotify-this-song" + " to retrieve information on your favorite songs!");
-        console.log("my-tweets" + " to find out what is on Kyle\'s mind!");
-        console.log("movie-this" + " to retrieve information on your favorite movies!");
-        console.log("Hope you enjoy this little app of mine!!");
+        console.log("\tdo-what-it-says" + " - to add any command to the random text file.");
+        console.log("\tspotify-this-song" + " - to retrieve information on your favorite songs!");
+        console.log("\tmy-tweets" + " - to find out what is on Kyle\'s mind!");
+        console.log("\tmovie-this" + " - to retrieve information on your favorite movies!");
+        console.log("\tHope you enjoy this little app of mine!!");
         }
     }
 }
